8732f6114ac4266184acc4f0e296b93a17148e43
threadez <laxmi.gumreddy@threadez.com>
2021-06-26 06:28:51 -0400

    sh changes

--- jenkins-terraform/install_jenkins.sh
+++ jenkins-terraform/install_jenkins.sh
@@ -2,6 +2,10 @@
 
 yum update -y
 
+sudo yum install -y yum-utils && sudo yum-config-manager --add-repo https://rpm.releases.hashicorp.com/AmazonLinux/hashicorp.repo && sudo yum -y install terraform
+
+yum install git -y
+
 wget -O /etc/yum.repos.d/jenkins.repo     https://pkg.jenkins.io/redhat-stable/jenkins.repo
 
 rpm --import https://pkg.jenkins.io/redhat-stable/jenkins.io.key
